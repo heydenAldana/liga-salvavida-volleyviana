8a58106546cbaa34733f8ff35b1d9b6ecee1356f
heydenAldana <heyden.aldana@unitec.edu>
2026-08-11 08:01:20 -0600

    Agregado torneos a enrutador, corregido mistypos

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const equiposRouter = require('./equipo.router');
-const jugadoresRouer = require('./jugador.router');
+const jugadoresRouter = require('./jugador.router');
+const torneoRouter = require('./torneo.router');
 
 const routerApi = (app) => {
   const router = express.Router();
   app.use('/api/v1', router);
 
   router.use('/equipo', equiposRouter);
-  router.use('/jugador', jugadoresRouer);
+  router.use('/jugador', jugadoresRouter);
+  router.use('/torneo', torneoRouter)
 }
 
 module.exports = routerApi;
