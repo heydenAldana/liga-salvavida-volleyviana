5b3f74c14f601dd67bb75cab1a256bc253a7d874
heydenAldana <heyden.aldana@unitec.edu>
2026-08-10 23:07:31 -0600

    Agregado jugadores a enrutador

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const equiposRouter = require('./equipo.router');
+const jugadoresRouer = require('./jugador.router');
 
 const routerApi = (app) => {
   const router = express.Router();
   app.use('/api/v1', router);
 
   router.use('/equipo', equiposRouter);
+  router.use('/jugador', jugadoresRouer);
 }
 
 module.exports = routerApi;
