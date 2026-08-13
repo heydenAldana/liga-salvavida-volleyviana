4cb13079e65edfc72c8ae56c7990702f22d127e5
heydenAldana <heyden.aldana@unitec.edu>
2026-08-12 19:10:49 -0600

    Agregado un nuevo URL de una API gratuita sin necesidad de una API KEY

--- backend/routes/AniDB.router.js
+++ backend/routes/AniDB.router.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 let animes = [];
 
-router.get('/animes', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const response = await fetch('http://anidb.net/api/anime-titles.xml.gz');
+    const response = await fetch('https://api.jikan.moe/v4/anime');
+    if (!response.ok)
+      throw new Error(`[Error] Error HTTP. Status: ${response.data}`);
     const data = await response.json();
     res.json(data)
   } catch (error) {
